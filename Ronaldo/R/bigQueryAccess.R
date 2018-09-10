b4503ff8e99b25ab5cb398ff30252cc2e9d35d3d
vkumra-broad <vkumra@broadinstitute.org>
2018-09-10 15:57:16 -0400

    Initial commit

---
+++ Ronaldo/R/bigQueryAccess.R
@@ -0,0 +1,16 @@
+#' Function to get the token for bigquery access
+#
+#' @return the token to access BigQuery
+getToken <-  function() {
+  token <- system("gcloud auth application-default print-access-token", intern=TRUE)
+  bearer <- sprintf("Bearer %s", token)
+  project <- Sys.getenv("GOOGLE_PROJECT")
+  samHost <- "sam.dsde-prod.broadinstitute.org"  # note this is PROD; replace with dev Sam in a dev notebook
+  samUrl <- sprintf("https://%s/api/google/user/petServiceAccount/%s/key", samHost, project)
+  json <- httr::GET(
+    url = samUrl,
+    httr::content_type_json(),
+    httr::add_headers(Authorization = bearer))
+  key <- httr::content(json, "text", encoding = "UTF-8")
+  return(key)
+}
